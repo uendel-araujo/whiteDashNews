c0d0a6dd3efc5571fd7a6310efacef3984777b9c
Uendel Araujo <dendellaraujo@gmail.com>
2021-12-19 16:43:46 -0300

    Realizado a insercao da biblioteca JoseNeto e modificacao da pagina Create

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -4,31 +4,27 @@
   <h1 class="d-flex justify-content-center mb-4 mt-4">Create Your News</h1>
   <div class="card">
     <div class="card-body">
-      <form method="POST" action="{{route('news.store')}}">
+      {!!Form::open()->post()->route('news.store')!!}
         @csrf
         
         <div class="form-group">
-          <label for="Input_Author_Name">Author Name</label>
-          <input type="text" name="author_name" class="form-control" id="Input_Author_Name"  placeholder="Uendel Ives">
+          {!!Form::text("author_name", "Author Name")!!}
         </div>
         <div class="form-group mt-2" >
-          <label for="Input_Title">News Title</label>
-          <input type="text" name="title" class="form-control" id="Input_Title"  placeholder="The Flu H2N3">
+          {!!Form::text("title", "Title")!!}
         </div>
         <div class="form-group mt-2">
-          <label for="Input_Sutitle">News Subtitle</label>
-          <input type="text" name="subtitle" class="form-control" id="Input_Subtitle"  placeholder="The firts case of H2N3 in Alagoinhas - BA">
+          {!!Form::text("subtitle", "Subtitle")!!}
         </div>
         
         <div class="form-group mt-2">
-          <label for="Textarea_News_Description">News Description</label>
-          <textarea  name="description" class="form-control" id="Textarea_News_Description"  rows="10"></textarea>
+          {!!Form::textarea("description", "News Description")!!}
         </div>
 
         <div class="form-group mt-4 mb-3">
-          <input class="btn btn-success" type="submit" value="Create News">
+          {!!Form::submit("Create News")!!}
         </div>
-      </form>
+      {!!Form::close()!!}
     </div>
   </div>  
 @endsection
--- resources/views/news/edit.blade.php
+++ resources/views/news/edit.blade.php
@@ -10,20 +10,20 @@
         
         <div class="form-group">
           <label for="Input_Author_Name">Author Name</label>
-          <input type="text" name="author_name" class="form-control" id="Input_Author_Name"  placeholder="Uendel Ives" value="{{$item->author_name}}">
+          <input type="text" name="author_name" class="form-control" id="Input_Author_Name"  placeholder="Uendel Ives" value="{{$item->author_name}}" required>
         </div>
         <div class="form-group mt-2" >
           <label for="Input_Title">News Title</label>
-          <input type="text" name="title" class="form-control" id="Input_Title"  placeholder="The Flu H2N3" value="{{$item->title}}">
+          <input type="text" name="title" class="form-control" id="Input_Title"  placeholder="The Flu H2N3" value="{{$item->title}}" required>
         </div>
         <div class="form-group mt-2">
           <label for="Input_Sutitle">News Subtitle</label>
-          <input type="text" name="subtitle" class="form-control" id="Input_Subtitle"  placeholder="The firts case of H2N3 in Alagoinhas - BA" value="{{$item->subtitle}}">
+          <input type="text" name="subtitle" class="form-control" id="Input_Subtitle"  placeholder="The firts case of H2N3 in Alagoinhas - BA" value="{{$item->subtitle}}" required>
         </div>
         
         <div class="form-group mt-2">
           <label for="Textarea_News_Description">News Description</label>
-          <textarea  name="description" class="form-control" id="Textarea_News_Description">{{$item->description}}</textarea>
+          <textarea  name="description" class="form-control" id="Textarea_News_Description" required>{{$item->description}}</textarea>
         </div>
 
         <div class="form-group mt-4 mb-3">
